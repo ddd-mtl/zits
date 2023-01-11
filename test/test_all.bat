d9b307e5b543f879d12dcf2675f2c49e6c5d835f
damien <doute.d@gmail.com>
2023-01-11 11:05:49 -0500

    Clearer test files

--- test/test_all.bat
+++ test/test_all.bat
@@ -1,4 +1,4 @@
-cargo run -- -i test\\struct\\rust.rs -o test\\struct\\struct.ts
-cargo run -- --default-zome-name useless -i test\\type\\rust.rs -o test\\type\\type.ts
-cargo run -- -i test\\enum\\rust.rs -o test\\enum\\enum.ts
-cargo run -- -i test\\fn\\rust.rs -o test\\fn\\fn.ts
+cargo run -- --no-imports -i test\\struct\\input.rs -o test\\struct\\output.ts
+cargo run -- --default-zome-name useless -i test\\type\\input.rs -o test\\type\\output.ts
+cargo run -- --no-imports -i test\\enum\\input.rs -o test\\enum\\output.ts
+cargo run -- --no-imports -i test\\fn\\input.rs -o test\\fn\\output.ts
--- test/test_all.sh
+++ test/test_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo run -- -i test/struct/rust.rs -o test/struct/test.d.ts
-cargo run -- -i test/type/rust.rs -o test/type/test.d.ts
-cargo run -- -i test/enum/rust.rs -o test/enum/test.d.ts
-cargo run -- -i test/fn/rust.rs -o test/fn/test.d.ts
+cargo run -- --no-imports -i test/struct/input.rs -o test/struct/output.ts
+cargo run -- --default-zome-name useless -i test/type/input.rs -o test/type/output.ts
+cargo run -- --no-imports -i test/enum/input.rs -o test/enum/output.ts
+cargo run -- --no-imports -i test/fn/input.rs -o test/fn/output.ts
